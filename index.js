19b714f5929ea21328426debfe5b5e63f1cb99b3
Caio1213 <caiofxpgm@gmail.com>
2024-09-10 17:38:52 -0300

    edit js add object meta, attributes and function of the same

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Olá, mundo!");
+
+
+let meta = {
+    objetivo: "Estudar e praticar 2 horas por dia", // atributos do objeto ou propriedades
+    estado: false, //atributos do objeto ou propriedades
+    apresentarMeta: () => { //metódo do objeto, função/ação que ele realiza 
+        console.log(`Objetivo a ser realizado: ${meta.objetivo}. Meta concluida? ${meta.estado}`);
+    },
+}
+console.log(meta.isChecked());
